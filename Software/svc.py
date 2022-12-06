1b605df2261822d84156ab0ac915e14214465d1d
Kevin Brockman <kevin.brockman@zurn.com>
2022-12-05 20:43:15 -0600

    We're successfully getting a prediction, next time see how good it is with the statistics and make sure everything is correct.

--- Software/svc.py
+++ Software/svc.py
@@ -5,9 +5,16 @@ from sklearn.svm import SVC
 from collections import Counter
 from sklearn.metrics import classification_report,confusion_matrix
 from sklearn.model_selection import GridSearchCV
+import numpy as np
 
-def svcmodel(data):
-	X_train, X_test, y_train, y_test = train_test_split(data['Content'], data['Class'], test_size=0.20, random_state = 50)    
+def svcmodel(df):
+	# print(df.shape[0])
+	# print(len(df["Content"].values[0]))
+	# vectors = np.zeros(shape=(df.shape[0],len(df["Content"].values[0])))
+	col_list = df.Content.values.tolist()
+	vectorarray = np.array(col_list)
+
+	X_train, X_test, y_train, y_test = train_test_split(vectorarray, df['Class'], test_size=0.20, random_state = 50)    
 	clf = SVC(kernel='linear').fit(X_train, y_train)
 
 	predictions = clf.predict(X_test)
